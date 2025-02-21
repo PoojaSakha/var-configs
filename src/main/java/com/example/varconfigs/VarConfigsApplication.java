f2d98ccbd18fd99d04fd36d974321e797916d093
pooja <poojaaurade80@gmail.com>
2025-02-21 15:40:17 +0530

    this is frist commit

---
+++ src/main/java/com/example/varconfigs/VarConfigsApplication.java
@@ -0,0 +1,13 @@
+package com.example.varconfigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarConfigsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarConfigsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/varconfigs/VarConfigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.varconfigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarConfigsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
