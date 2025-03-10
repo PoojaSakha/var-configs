c3856719f185fa838e5e2e769e43946f63581eea
pooja <poojaaurade80@gmail.com>
2025-03-10 15:10:44 +0530

    added jenkinsfile new version changes triggering useversionjob

--- jenkins_webhook.sh
+++ jenkins_webhook.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# Jenkins credentials
-JENKINS_USER="pooja aurade"
-JENKINS_TOKEN="112c8b5db715317b254b067d5f08102c9a"
-JENKINS_URL="http://localhost:8080"
-
-# Get the crumb
-CRUMB=$(curl -u "${JENKINS_USER}:${JENKINS_TOKEN}" -s "${JENKINS_URL}/crumbIssuer/api/json" | jq -r '.crumb')
-
-# Print the crumb to verify it is retrieved correctly
-echo "Crumb: ${CRUMB}"
-
 # Read the callback URL from the file
 CALLBACK_URL=$(cat callback_url.txt)
 
-# Make the POST request with the crumb
-curl -X POST -H 'Content-Type: application/json' -H "Jenkins-Crumb:${CRUMB}" -d "{\"callback\":\"${CALLBACK_URL}\"}" "${JENKINS_URL}/webhook"
+# Data to send in the POST request
+DATA='{"status":"success"}'
+
+# Send the POST request
+curl -X POST -H 'Content-Type: application/json' -d "${DATA}" "${CALLBACK_URL}"
