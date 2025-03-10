3d909bc7dc9be8ecab41fe64c3874751de06ae09
pooja <poojaaurade80@gmail.com>
2025-03-10 12:17:34 +0530

    added jenkinsfile new version changes triggering useversionjob

---
+++ src/main/java/com/example/varconfigs/Examples.java
@@ -0,0 +1,32 @@
+//package com.example.varconfigs;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class Examples {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, world!");
+//
+//        //Arrays.asList("1","b","c").stream().forEach(System.out::println);
+//
+//       // List<String> list=Arrays.asList("pooja","pallavi","Anand");
+//        //list.stream().findFirst().ifPresent(System.out::println);
+//        //list.stream().forEach(System.out::println);
+//        List<String> list1=Arrays.asList("123","wed","wedfr");
+////        long i = list1.stream().count();
+////        System.out.println(i); //3
+////        System.out.println(list1.stream().count()); //3
+//
+////  //for finding even in the list
+////      List<Integer> list2 =  Arrays.asList(1,2,3,4,5,6,7,8);
+////      list2.stream().filter(i->i%2==0).forEach(System.out::println);
+//
+//        List<Integer> list3 =Arrays.asList(123,898,553,564,589);
+//        list3.stream().filter(I->I%2==0).forEach(System.out::println);
+//       // print with 5
+//        list3.stream().map(String::valueOf)
+//                .filter(s -> s.startsWith("5"))
+//                .forEach(System.out::println);
+//
+//    }
+//}
